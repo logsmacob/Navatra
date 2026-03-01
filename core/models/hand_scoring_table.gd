5f63ec890a2a9ecf50abdb08377cec0d03a0abdf
logsmacob <LMACURDY315@GMAIL.COM>
2026-03-01 14:10:36 -0700

    fixed event bus

--- core/models/hand_scoring_table.gd
+++ core/models/hand_scoring_table.gd
@@ -3,25 +3,25 @@ class_name HandScoringTable
 # NOTE: Foundation values that can later be moved to external resources.
 # The keys are HandEvaluatorModel.HandType values.
 const BASE_BY_TYPE := {
-	HandEvaluatorModel.HandType.HIGH_DIE: 2,
+	HandEvaluatorModel.HandType.HIGH_DIE: 5,
 	HandEvaluatorModel.HandType.ONE_PAIR: 5,
-	HandEvaluatorModel.HandType.TWO_PAIR: 9,
-	HandEvaluatorModel.HandType.THREE_OF_A_KIND: 13,
+	HandEvaluatorModel.HandType.TWO_PAIR: 10,
+	HandEvaluatorModel.HandType.THREE_OF_A_KIND: 15,
 	HandEvaluatorModel.HandType.STRAIGHT: 20,
-	HandEvaluatorModel.HandType.FULL_HOUSE: 24,
-	HandEvaluatorModel.HandType.FOUR_OF_A_KIND: 30,
-	HandEvaluatorModel.HandType.FIVE_OF_A_KIND: 45
+	HandEvaluatorModel.HandType.FULL_HOUSE: 25,
+	HandEvaluatorModel.HandType.FOUR_OF_A_KIND: 25,
+	HandEvaluatorModel.HandType.FIVE_OF_A_KIND: 30
 }
 
 const MULT_BY_TYPE := {
-	HandEvaluatorModel.HandType.HIGH_DIE: 0.0,
-	HandEvaluatorModel.HandType.ONE_PAIR: 0.1,
-	HandEvaluatorModel.HandType.TWO_PAIR: 0.25,
-	HandEvaluatorModel.HandType.THREE_OF_A_KIND: 0.5,
-	HandEvaluatorModel.HandType.STRAIGHT: 0.8,
-	HandEvaluatorModel.HandType.FULL_HOUSE: 1.0,
-	HandEvaluatorModel.HandType.FOUR_OF_A_KIND: 1.5,
-	HandEvaluatorModel.HandType.FIVE_OF_A_KIND: 2.5
+	HandEvaluatorModel.HandType.HIGH_DIE: 2,
+	HandEvaluatorModel.HandType.ONE_PAIR: 2,
+	HandEvaluatorModel.HandType.TWO_PAIR: 3,
+	HandEvaluatorModel.HandType.THREE_OF_A_KIND: 4,
+	HandEvaluatorModel.HandType.STRAIGHT: 6,
+	HandEvaluatorModel.HandType.FULL_HOUSE: 8,
+	HandEvaluatorModel.HandType.FOUR_OF_A_KIND: 10,
+	HandEvaluatorModel.HandType.FIVE_OF_A_KIND: 14
 }
 
 static func get_base(hand_type: int) -> int:
