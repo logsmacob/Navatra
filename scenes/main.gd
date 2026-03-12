2b001f4f903ffb436808616556668ea942eec876
logsmacob <LMACURDY315@GMAIL.COM>
2026-03-12 06:44:06 -0600

    asf

--- scenes/main.gd
+++ scenes/main.gd
@@ -6,11 +6,9 @@ extends Control
 @onready var current_hand_points_label: Label = $VBoxContainer/CurrentHandPoints
 @onready var hand_type_label: Label = $VBoxContainer/HandType
 @onready var hand_type_value_label: Label = $VBoxContainer/HandTypeValue
-@onready var hands_left_label: Label = $VBoxContainer/HandaLeft
+@onready var hands_left_leabel: Label = $VBoxContainer/HandaLeft
 @onready var rolls_left_label: Label = $VBoxContainer/RollsLeft
 @onready var reward_shop: Control = $RewardShop
-
-const HAND_FINISH_DELAY_SECONDS: float = 1.0
 
 var score_manager: ScoreManager
 var reward_service: RewardService
@@ -37,7 +35,8 @@ func _on_played_hand_ready(hand_data: DiceHand) -> void:
 
 	if not score_manager.can_play_hand():
 		GameState.consume_hand()
-		await _finish_hand_after_delay()
+		await get_tree().create_timer(1).timeout
+		hand._on_played_hand_finish()
 		return
 
 	score_manager.play_hand()
@@ -47,7 +46,8 @@ func _on_played_hand_ready(hand_data: DiceHand) -> void:
 	GameState.apply_score_to_quota(applied_score)
 	GameState.consume_hand()
 
-	await _finish_hand_after_delay()
+	await get_tree().create_timer(1).timeout
+	hand._on_played_hand_finish()
 
 func _get_played_hand_name() -> String:
 	var breakdown := score_manager.get_last_breakdown()
@@ -95,10 +95,6 @@ func _refresh_hand_preview() -> void:
 
 	score_manager.preview_hand(current_hand.to_array())
 
-func _finish_hand_after_delay() -> void:
-	await get_tree().create_timer(HAND_FINISH_DELAY_SECONDS).timeout
-	hand._on_played_hand_finish()
-
 func ui_update(state: Dictionary = {}) -> void:
 	if state.is_empty():
 		state = GameState.get_round_state()
@@ -113,5 +109,5 @@ func ui_update(state: Dictionary = {}) -> void:
 	current_hand_points_label.text = "Current Hand Points: %d" % final_score
 	hand_type_label.text = "Hand Type: %s" % hand_name
 	hand_type_value_label.text = "Hand Type Value: %d" % type_total
-	hands_left_label.text = "Hands Left: %d" % int(state.get("hands_remaining", 0))
+	hands_left_leabel.text = "Hands Left: %d" % int(state.get("hands_remaining", 0))
 	rolls_left_label.text = "Rolls Left: %d" % int(state.get("rerolls_remaining", 0))
