8737cc611115ad7641237c9714a27fa46201b9b3
logsmacob <LMACURDY315@GMAIL.COM>
2026-03-13 12:15:37 -0600

    jhgf

--- features/hand_type_upgrades/hand_type_upgrades.gd
+++ features/hand_type_upgrades/hand_type_upgrades.gd
@@ -6,22 +6,22 @@ signal reroll_requested
 @onready var upgrades_container: VBoxContainer = $VBoxContainer/UpgradeList
 @onready var reroll_button: Button = $VBoxContainer/RerollButton
 
+@export var buttons: Array[Button]
+
 func _ready() -> void:
 	reroll_button.pressed.connect(_on_reroll_pressed)
 
 func show_upgrades(upgrades: Array[HandTypeUpgradeDefinition]) -> void:
-	for child in upgrades_container.get_children():
-		child.queue_free()
 
+	var i = 0
 	for upgrade in upgrades:
 		if upgrade == null:
 			continue
 
-		var button := Button.new()
+		var button := buttons[i]
+		i += 1
 		button.text = "%s\n%s" % [upgrade.get_title(), upgrade.get_description()]
-		button.custom_minimum_size = Vector2(0, 110)
 		button.pressed.connect(_on_upgrade_pressed.bind(upgrade))
-		upgrades_container.add_child(button)
 
 	visible = true
 
