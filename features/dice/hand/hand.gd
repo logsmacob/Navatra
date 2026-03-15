e725718d4c7d828bbf04cddec80365f57e07ce4f
logsmacob <LMACURDY315@GMAIL.COM>
2026-03-14 23:23:38 -0600

    asdf

--- features/dice/hand/hand.gd
+++ features/dice/hand/hand.gd
@@ -80,8 +80,8 @@ func get_scoring_material_currency_bonus() -> int:
 	for die_ui: DieUI in scoring_dice:
 		if die_ui == null or die_ui.die == null or die_ui.die.data == null:
 			continue
-		var material := str(die_ui.die.data.material)
-		total_bonus += int(GameState.MATERIAL_CURRENCY_BONUS.get(material, 0))
+		var _material := str(die_ui.die.data.material)
+		total_bonus += int(GameState.MATERIAL_CURRENCY_BONUS.get(_material, 0))
 	return total_bonus
 
 func _on_played_hand_finish() -> void:
