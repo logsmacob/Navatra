8bd6e0fca51abf0de391e8839d82d092cd17422e
Logan Macurdy <LMACURDY315@GMAIL.COM>
2026-03-15 16:11:19 -0600

    Refactor hand into modular pool and bonus services

--- features/dice/hand/hand.gd
+++ features/dice/hand/hand.gd
@@ -1,55 +1,37 @@
 extends Control
 
-@onready var hand_container = $HBoxContainer/Panel/HandContainer
-@onready var hand_animator: Node = $HandAnimator
+@onready var hand_container: Node = $HBoxContainer/Panel/HandContainer
+@onready var hand_animator: HandAnimator = $HandAnimator
 @onready var hand_scoring_selector: HandScoringSelector = $HandScoringSelector
+@onready var hand_dice_pool: HandDicePool = $HandDicePool
+@onready var hand_currency_bonus_service: HandCurrencyBonusService = $HandCurrencyBonusService
+
 @export var die_ui_scene: PackedScene
 @export var dice_per_hand: int = 5
 
 var is_hand_ready: bool = false
 
+var dice: Array[DieUI]:
+	get:
+		return hand_dice_pool.get_dice()
+
 signal setup_complete
 signal played_hand_ready(hand: DiceHand)
 signal played_hand_finished
 
-var dice: Array[DieUI] = []
-
 func _ready() -> void:
-	_build_hand(dice_per_hand)
-
+	hand_dice_pool.setup(die_ui_scene, dice_per_hand, hand_container)
 	setup_complete.emit()
 	is_hand_ready = true
-
-func _build_hand(dice_count: int) -> void:
-	var configured_hand: Array[Dictionary] = GameState.get_player_hand()
-	if configured_hand.is_empty():
-		GameState.initialize_player_hand(dice_count)
-		configured_hand = GameState.get_player_hand()
-
-	for i in range(dice_count):
-		var die_ui: DieUI = die_ui_scene.instantiate()
-		hand_container.add_child(die_ui)
-
-		var die := DieInstance.create_standard_d6()
-		if i < configured_hand.size():
-			var die_data: Dictionary = configured_hand[i]
-			var faces: Array = die_data.get("faces", [])
-			if faces.size() == 6:
-				die.set_face_values(faces)
-			die.data.material = str(die_data.get("material", GameState.DIE_MATERIAL_STANDARD))
-
-		die_ui.set_die(die)
-		die_ui.roll_if_not_selected(0)
-		dice.append(die_ui)
 
 func _on_roll_pressed() -> void:
 	if not is_hand_ready:
 		return
-		
-	if is_hand_ready and GameState.consume_reroll():
+
+	if GameState.consume_reroll():
 		roll_hand()
 
-func roll_hand():
+func roll_hand() -> void:
 	if hand_animator.is_roll_finished:
 		is_hand_ready = false
 		await hand_animator.roll_hand()
@@ -67,8 +49,7 @@ func _on_play_pressed() -> void:
 		return
 
 	await hand_animator.play_hand(hand_scoring_selector.get_scoring_dice(dice))
-	for die in dice:
-		die.is_selected = false
+	hand_dice_pool.clear_selection()
 	played_hand_ready.emit(hand_scoring_selector.build_dice_hand(dice))
 
 func get_current_hand() -> DiceHand:
@@ -76,13 +57,7 @@ func get_current_hand() -> DiceHand:
 
 func get_scoring_material_currency_bonus() -> int:
 	var scoring_dice := hand_scoring_selector.get_scoring_dice(dice)
-	var total_bonus := 0
-	for die_ui: DieUI in scoring_dice:
-		if die_ui == null or die_ui.die == null or die_ui.die.data == null:
-			continue
-		var _material := str(die_ui.die.data.material)
-		total_bonus += int(GameState.MATERIAL_CURRENCY_BONUS.get(_material, 0))
-	return total_bonus
+	return hand_currency_bonus_service.get_scoring_material_currency_bonus(scoring_dice)
 
 func _on_played_hand_finish() -> void:
 	played_hand_finished.emit()
---
+++ features/dice/hand/hand_currency_bonus_service.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name HandCurrencyBonusService
+
+func get_scoring_material_currency_bonus(scoring_dice: Array[DieUI]) -> int:
+	var total_bonus := 0
+	for die_ui: DieUI in scoring_dice:
+		if die_ui == null or die_ui.die == null or die_ui.die.data == null:
+			continue
+		var material := str(die_ui.die.data.material)
+		total_bonus += int(GameState.MATERIAL_CURRENCY_BONUS.get(material, 0))
+	return total_bonus
