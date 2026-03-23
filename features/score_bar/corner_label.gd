2465cd2ee750e9fbaaabe398f157ae94e99e688b
Logan Macurdy <LMACURDY315@GMAIL.COM>
2026-03-23 06:34:56 -0600

    Refactor score bar panel ownership

--- features/score_bar/corner_label.gd
+++ features/score_bar/corner_label.gd
@@ -1,1 +1,11 @@
 extends PanelContainer
+class_name CornerLabel
+
+@onready var _round_index_label: Label = get_node("left column/RoundIndex") as Label
+@onready var _marble_label: Label = get_node("left column/Marbles") as Label
+
+func set_round(round_index: int, max_rounds: int) -> void:
+	_round_index_label.text = "Round %d/%d" % [round_index, max_rounds]
+
+func set_marbles(amount: int) -> void:
+	_marble_label.text = "Marbles: %d" % amount
